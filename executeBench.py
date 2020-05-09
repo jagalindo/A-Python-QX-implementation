c44685cd061b1e6d078341d7f296771166c18fab
José Á. Galindo <jagalindo@us.es>
2020-05-09 18:54:08 +0200

    Parameters added

--- executeBench.py
+++ executeBench.py
@@ -5,9 +5,8 @@ for i in cc:
     modelPath="./cnf/bench/model_"+i+".cnf"
     for j in range(10):
         productPath="./cnf/bench/prod-"+i+"-"+str(j)+".prod"
-        #create product in cnf
-        for lmax in ["1","2"] :
-            for solver in ["Glucose3","Sat4j"]
+        for lmax in ["1","2"]:
+            for solver in ["Glucose3","Sat4j"]:
                 for difficulty in ["0","300","400","500"]:
                     os.system("python3 ./qx.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " result.csv")
                     os.system("python3 ./qx_parallel_mp.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " result.csv")
